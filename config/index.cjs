6cb8b52710dab6e98bc9804810dc0cc109c2db83
sghyj0606 <115475147+sghyj0606@users.noreply.github.com>
2022-10-24 17:04:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,21 +19,6 @@ const USER_CONFIG = {
     // 天行API KEY，如果使用天行API则需要填写此项
     key: '2abe96e5a3b50a36fb461abd56aa4ca4',
     
-    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
-    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    morningGreeting: true,
-  
-    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    eveningGreeting: true,
-  
-    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
-    weather: 3,
-  
-    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
-    networkHot: 3,
-  
-    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
-    networkHotType: 'default',
   },
   
   /** 是否给文字设置多彩颜色, 和emoji不兼容 */
